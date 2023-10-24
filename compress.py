c1385f76f499b160ead47cb775b1b063e05a14ab
Yakup Allakov <72393805+imallakov@users.noreply.github.com>
2023-10-24 15:23:54 +0500

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,5 +1,6 @@
 from ascii_to_dict import get_ascii_to_dict
 
+
 def archiver(text: str) -> str:
     if len(text) < 3:
         return text
@@ -7,8 +8,8 @@ def archiver(text: str) -> str:
     pos: int = 0
     working_str: str = ""
     table_index: int = 256
-    max_index = 16 #in power of 2, e.g. 2**max_index = 2**16 = 65536, we allow to store 65536 tables in dictionary
-    current_index = 9 # also in power of 2, e.g. 2**current_index = 2**9 = 512
+    max_index = 16  # in power of 2, e.g. 2**max_index = 2**16 = 65536, we allow to store 65536 tables in dictionary
+    current_index = 9  # also in power of 2, e.g. 2**current_index = 2**9 = 512
     bits: list = []
     for pos in range(len(text)):
         current_char: str = text[pos]
@@ -20,29 +21,29 @@ def archiver(text: str) -> str:
                 table_index += 1
                 if table_index == 2**current_index:
                     current_index += 1
-            bits.append( int_to_bits(char_to_code[augmented_work_str[:-1]]) )
+            bits.append(int_to_bits(char_to_code[augmented_work_str[:-1]]))
             working_str = current_char
         else:
             working_str = augmented_work_str
-    bits.append( int_to_bits(char_to_code[working_str]) )
-    return get_one_str(bits,current_index)
+    bits.append(int_to_bits(char_to_code[working_str]))
+    return get_one_str(bits, current_index)
 
 
 def get_one_str(bits: list, index: int) -> str:
     ans: str = ""
     if index < 16:
-        ans += '0'
+        ans += "0"
     ans += int_to_bits(index)
     for bit in bits:
         for _ in range(index - len(bit)):
-            ans += '0'
+            ans += "0"
         ans += str(bit)
     return ans
 
 
 def int_to_bits(num: int) -> str:
     byte: str = ""
-    while num>0:
+    while num > 0:
         rem = num % 2
         byte += str(rem)
         num //= 2
